4cc76a374315d3e111070b321bb3b0506e6c5d51
Lineod <2510575128@qq.com>
2026-02-08 00:50:16 +0800

    correct path

--- simple-demo/src/heads_feet.js
+++ simple-demo/src/heads_feet.js
@@ -3,7 +3,7 @@ import './style.scss'
 document.querySelector('#heads').innerHTML = `<head>
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml"
-        href="https://cdn.jsdelivr.net/gh/Lineod/wiki_assets/Template_practice/icon.svg" />
+        href="https://cdn.jsdelivr.net/gh/Lineod/wiki_assets/Template_practice/img/icon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
