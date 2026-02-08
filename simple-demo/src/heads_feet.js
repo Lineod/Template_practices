11690f4a20bde884f55977ba107af204193acb59
Lineod <2510575128@qq.com>
2026-02-08 10:04:56 +0800

    faster CDN

--- simple-demo/src/heads_feet.js
+++ simple-demo/src/heads_feet.js
@@ -3,9 +3,9 @@ import './style.scss'
 document.querySelector('#heads').innerHTML = `<head>
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml"
-        href="https://cdn.jsdelivr.net/gh/Lineod/wiki_assets/Template_practice/img/icon.svg" />
+        href="https://gcore.jsdelivr.net/gh/Lineod/wiki_assets/Template_practice/img/icon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="https://cdn.bootcdn.net/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="/src/style.scss">
     <title>Another Page</title>
