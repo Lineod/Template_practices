2f0cea0583730b43808120c3b37eaa384bce7710
Lineod <2510575128@qq.com>
2026-02-08 23:17:50 +0800

    bugfix:404.html

--- simple-demo/vite.config.js
+++ simple-demo/vite.config.js
@@ -10,31 +10,29 @@ export default defineConfig({
         outDir: 'dist',
         assetsDir: 'assets',
         sourcemap: true,
-        copyPublicDir: true
-        // rollupOptions: {
-        //     output: {
-        //         assetFileNames: 'assets/[name]-[hash][extname]',
-        //         chunkFileNames: 'assets/[name]-[hash].js',
-        //         entryFileNames: 'assets/[name]-[hash].js'
-        //     }
-        // }
-    },
+        copyPublicDir: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            }
+        },
 
-    // CSS 预处理配置
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
+        // CSS 预处理配置
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
           @use "./src/styles/style" as *;
         `
+                }
             }
-        }
-    },
+        },
 
-    // 开发服务器配置
-    server: {
-        port: 3000,
-        open: true,
-        hot: true
+        // 开发服务器配置
+        server: {
+            port: 3000,
+            open: true,
+            hot: true
+        }
     }
 })
